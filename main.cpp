6a6caba71d6a1eba961ed39a965bb2789fdca664
Duarte Areias dos Reis <duarte.reis@criticaltechworks.com>
2022-10-19 15:51:54 +0100

    Added more POC - Read now calls a single time

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 
 // Proof of concept for sending and receiving an HTTP request and response
-void hello_world() {
+void client_hello_world() {
     SocketListener server(AF_INET, 8089);
     server.bind_to_address();
     server.start_listening();
@@ -15,7 +15,19 @@ void hello_world() {
     std::cout << server.read_from_connection(NULL) << std::endl;
 }
 
+// Proof of concept for sending and receiving an HTTP request and response
+void browser_hello_world() {
+    SocketListener server(AF_INET, 8089);
+    server.bind_to_address();
+    server.start_listening();
+    
+    server.accept_connections();
+    std::cout << server.read_from_connection(NULL) << std::endl;
+    server.write_to_connection(NULL, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Lenght: 14\r\n\r\nHello, world!");
+}
+
 int main() {
-    hello_world();
+    //client_hello_world();
+    browser_hello_world();
     return 0;
 }
--- srcs/SocketConnection.cpp
+++ srcs/SocketConnection.cpp
@@ -43,19 +43,27 @@ std::string SocketConnection::read_connection(void) {
 	char buff[READING_BUFFER + 1]; // This will probably not be a fized size
 	size_t bytes_read;
 	std::string temp;
-	int maximum_iterations = 65535 / READING_BUFFER; // This is equivalent to having a 64Kb packet (TCP max packet size)
+	//int maximum_iterations = 65535 / READING_BUFFER; // This is equivalent to having a 64Kb packet (TCP max packet size)
 
-	while ((bytes_read = recv(fd, &buff, READING_BUFFER, 0)) > 0) {
-		if (!maximum_iterations)
-			break;
+	// while ((bytes_read = recv(fd, &buff, READING_BUFFER, 0)) > 0) {
+	// 	if (!maximum_iterations)
+	// 		break;
+	// 	buff[bytes_read] = '\0';
+	// 	temp += buff;
+	// 	--maximum_iterations;
+	// 	if (bytes_read < READING_BUFFER)
+	// 		break;
+	// }
+	// if (maximum_iterations == 65535 / READING_BUFFER) // No loop was done
+		// return "No message in connection - RECV failed";
+
+	bytes_read = recv(fd, &buff, READING_BUFFER, 0);
+	if (bytes_read <= 0) {
+		return "No message in connection - RECV failed";
+	} else {
 		buff[bytes_read] = '\0';
 		temp += buff;
-		--maximum_iterations;
-		if (bytes_read < READING_BUFFER)
-			break;
 	}
-	if (maximum_iterations == 65535 / READING_BUFFER) // No loop was done
-		return "No message in connection - RECV failed";
 	return temp;
 }
 
