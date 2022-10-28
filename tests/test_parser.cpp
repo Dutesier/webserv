2bd20600388af540ad23e77f5fa4fa776eb8b0ea
Duarte Areias dos Reis <duarte.reis@criticaltechworks.com>
2022-10-28 12:35:17 +0100

    Provided pointer to object in Gtest Fixture to facilitate instance destruction

--- tests/test_parser.cpp
+++ tests/test_parser.cpp
@@ -9,14 +9,18 @@
 class test_parser : public ::testing::Test {
 
 	public:
-		void	SetUp() {};
-		void	TearDown() {};
+		void	SetUp() {
+			parser = new Parser();
+		};
+		void	TearDown() {
+			delete parser;
+		};
 		void	Reset() {
-			this->parser.set_listen(0);
-			this->parser.set_error(nullptr);
+			parser->set_listen(0);
+			parser->set_error(nullptr);
 		}
 	protected:
-		Parser	parser;
+		Parser*	parser;
 };
 
 TEST_F(test_parser, test_listen_directive) {
@@ -24,39 +28,39 @@ TEST_F(test_parser, test_listen_directive) {
 	std::vector<std::string>	command;
 
 	// Testing with empty command
-	EXPECT_FALSE(this->parser.listen_handler(command));
-	EXPECT_NE(this->parser.get_error(), nullptr);
+	EXPECT_FALSE(this->parser->listen_handler(command));
+	EXPECT_NE(this->parser->get_error(), nullptr);
 
 	// Testing with wrong command
 	this->Reset();
 	command.push_back("root");
 
-	EXPECT_FALSE(this->parser.listen_handler(command));
-	EXPECT_NE(this->parser.get_error(), nullptr);
+	EXPECT_FALSE(this->parser->listen_handler(command));
+	EXPECT_NE(this->parser->get_error(), nullptr);
 
 	// Testing with wrong number of arguments command
 	this->Reset();
 	command[0] = "listen";
 
-	EXPECT_FALSE(this->parser.listen_handler(command));
-	EXPECT_NE(this->parser.get_error(), nullptr);
+	EXPECT_FALSE(this->parser->listen_handler(command));
+	EXPECT_NE(this->parser->get_error(), nullptr);
 
 	// Testing with wrong arguments command
 	command.push_back("wrong");
 
-	EXPECT_FALSE(this->parser.listen_handler(command));
-	EXPECT_NE(this->parser.get_error(), nullptr);
+	EXPECT_FALSE(this->parser->listen_handler(command));
+	EXPECT_NE(this->parser->get_error(), nullptr);
 
 	command[1] = "-1";
 
-	EXPECT_FALSE(this->parser.listen_handler(command));
-	EXPECT_NE(this->parser.get_error(), nullptr);
+	EXPECT_FALSE(this->parser->listen_handler(command));
+	EXPECT_NE(this->parser->get_error(), nullptr);
 
 	// Testing successfull case
 	this->Reset();
 	command[1] = "443";
 
-	EXPECT_TRUE(this->parser.listen_handler(command));
-	EXPECT_EQ(this->parser.get_error(), nullptr);
-	EXPECT_EQ(this->parser.get_listen(), 443);
+	EXPECT_TRUE(this->parser->listen_handler(command));
+	EXPECT_EQ(this->parser->get_error(), nullptr);
+	EXPECT_EQ(this->parser->get_listen(), 443);
 }
