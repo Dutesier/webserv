56623d49436d013146e72df0ea3e221102c74234
mlanca-c <marialsottomayor@gmail.com>
2022-12-02 09:57:15 +0000

    sketch of HTTPHandler impl class

--- srcs/HTTPHandler.cpp
+++ srcs/HTTPHandler.cpp
@@ -10,9 +10,16 @@ void HTTPHandler::handle(TCPSocket* socket, int fd) {
 /* HTTPHandler::impl Class */
 HTTPHandler::impl::impl(TCPSocket* socket, int fd)
     : socket(socket), client(this->socket->connection(fd)) {
+
     std::string str = this->client->recv();
     std::cout << str << std::endl;
     this->client->send(str);
 }
+
+// HTTPHandler::impl::impl(TCPSocket* socket, int fd)
+//     : socket(socket), client(this->socket->connection(fd)) {
+// 	HTTPRequest req( this->client->recv() );
+//  	this->client->send(HTTPResponse(req).to_str);
+// }
 
 } // namespace webserv
