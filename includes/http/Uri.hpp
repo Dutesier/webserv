690dbd9e2b30710d45f2fc77e9810e2ddcbf2ceb
mlanca-c <marialsottomayor@gmail.com>
2023-01-16 11:47:38 +0000

    formatted code with .clang-format

--- includes/http/Uri.hpp
+++ includes/http/Uri.hpp
@@ -1,5 +1,5 @@
 #ifndef URI_HPP
-# define URI_HPP
+#define URI_HPP
 
 #include <string>
 
@@ -7,20 +7,20 @@ namespace webserv {
 
 struct Uri {
 
-	Uri(std::string uri);
-	~Uri(void);
+        Uri(std::string uri);
+        ~Uri(void);
 
-	std::string to_str(void);
-	std::string path_w_root(std::string root);
+        std::string to_str(void);
+        std::string path_w_root(std::string root);
 
-	std::string m_scheme;
-	std::string m_host;
-	std::string m_path;
-	std::string m_query;
+        std::string m_scheme;
+        std::string m_host;
+        std::string m_path;
+        std::string m_query;
 
-	std::string m_root;
+        std::string m_root;
 };
 
-}
+} // namespace webserv
 
 #endif /* URI_HPP */
